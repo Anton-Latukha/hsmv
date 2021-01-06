033e5ab42ca2de1793d9a045ee6058d1c89f9f25
Anton-Latukha <anton.latukha@gmail.com>
2021-01-06 23:23:00 +0200

    refactor code
    The main agenda is to make code more readable.

--- src/Hsmv.hs
+++ src/Hsmv.hs
@@ -13,65 +13,86 @@ import           Data.Maybe
 #if !MIN_VERSION_base(4,9,0)
 import           Data.Semigroup ((<>))
 #endif
+import           Data.Bool (bool)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Text.Regex.Applicative.Text
 
+
 --------------------------------------------------------------------------------
--- Regex replacements
+-- * Types
 
-applyToContents :: Bool -> Text -> Text -> Text -> Text
-applyToContents autoQualify from to =
-  replace (fmap renderImport (inImport autoQualify from mname)) . replace inHeader
-  where
-    inHeader = line <> string "module" <> spaces1 <> mname
-    mname = string from *> (pure to <> spaces1)
-
-spaces1 :: RE Char Text
-spaces1 = capture (some (psym isSpace))
-
-line :: RE Char Text
-line = capture (many (psym (\c -> c `elem` ['\n', '\r'])))
+type ModuleName = Text
+type DryRunFlag = Bool
+type AutoQualifyFlag = Bool
+type FileContents = Text
+type ImportSrcLine = Text
 
 data Import =
   Import
     { preamble :: Text
     , qualified :: Text
     , package :: Text
-    , name :: Text
+    , name :: ModuleName
     , as_ :: Text
     }
+
+
+--------------------------------------------------------------------------------
+-- * Regex helpers
+
+capture :: RE' a -> RE Char Text
+capture = fmap snd . withMatched
+
+-- | Capture all space symbols
+spaces1 :: RE Char Text
+spaces1 = capture $ some $ psym isSpace
+
+line :: RE Char Text
+line = capture $ many $ psym (`elem` ['\n', '\r'])
+
+mkRegexToImport :: RE Char Text -> RE Char Import
+mkRegexToImport regexModuleName =
+  Import
+    <$> preamble
+    <*> qualified
+    <*> pkgImport
+    <*> regexModuleName
+    <*> as_
+ where
+   preamble = line <> string "import" <> spaces1
+   qualified = captureRegex $ string "qualified"
+   pkgImport = captureRegex $ string "\"" *> few (psym (/= '"')) *> string "\""
+   as_ = captureRegex $ string "as"
+   captureRegex re' = fmap (fromMaybe "") $ optional $ capture (re' *> spaces1)
+
+
+--------------------------------------------------------------------------------
+-- * Import transformations
+
+inImport :: AutoQualifyFlag -> ModuleName -> RE Char Text -> RE Char Import
+inImport qualifyFlag fromName regexModuleName =
+  bool id (fmap $ qualify fromName) qualifyFlag $ mkRegexToImport regexModuleName
 
 renderImport :: Import -> Text
 renderImport Import {preamble, qualified, package, name, as_} =
   preamble <> qualified <> package <> name <> as_
 
-inImport :: Bool -> Text -> RE Char Text -> RE Char Import
-inImport autoQualify from mname =
-  fmap
-    qualify
-    (Import <$> preamble <*> qualified <*> pkgImport <*> mname <*> as_)
-  where
-    qualify i =
-      case i of
-        Import {as_ = "", name = name'}
-          | autoQualify -> i {as_ = " as " <> from <> maybe "" (const "\n") mstripped
-                             ,name = fromMaybe name' mstripped}
-          where mstripped = T.stripSuffix "\n" name'
-        _ -> i
-    preamble = line <> string "import" <> spaces1
-    pkgImport =
-      fmap
-        (fromMaybe "")
-        (optional
-           (capture
-              (string "\"" *> few (psym (/= '"')) *> string "\"" *> spaces1)))
-    qualified =
-      fmap (fromMaybe "") (optional (capture (string "qualified" *> spaces1)))
-    as_ = fmap (fromMaybe "") (optional (capture (string "as" *> spaces1)))
+qualify :: ModuleName -> Import -> Import
+qualify fromName i@Import{as_ = "", name = name'} =
+  i { name = fromMaybe name' mstripped
+    , as_ = " as " <> fromName <> maybe "" (const "\n") mstripped
+    }
+ where mstripped = T.stripSuffix "\n" name'
+qualify _ i = i
 
-capture :: RE' a -> RE Char Text
-capture = fmap snd . withMatched
+applyToContents :: AutoQualifyFlag -> ModuleName -> ModuleName -> FileContents -> FileContents
+applyToContents qualifyFlag fromName toName =
+  replace (fmap renderImport (inImport qualifyFlag fromName mname)) . replace inHeader
+  where
+    inHeader = line <> string "module" <> spaces1 <> mname
+    mname = string fromName *> (pure toName <> spaces1)
+
 
 #if !MIN_VERSION_regex_applicative(0,3,4)
 --------------------------------------------------------------------------------
